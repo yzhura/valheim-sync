c8bd2ce67e911fcdbd95bf20b169527a7fe83a4a
yzhura <zhuravlev.yaroslav@gmail.com>
2023-11-05 15:00:52 +0200

    notifications

--- sync.js
+++ sync.js
@@ -1,40 +1,44 @@
-import { readdirSync, copyFileSync } from "fs";
+import fs, { readdirSync, copyFileSync } from "fs";
 import { execSync } from "child_process";
 import { env } from "process";
+import notifier from "node-notifier";
 
 const REPO_URL = "https://github.com/yzhura/valheim-sync";
 const USER_PROFILE_PATH = env.USERPROFILE;
 const WORLDS_LOCAL_PATH = `${USER_PROFILE_PATH}/AppData/LocalLow/IronGate/Valheim/worlds_local`;
 const CURRENT_FOLDER = process.cwd();
-
-function showNotification(title, message) {
-  const psScript = `
-    [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
-    [System.Windows.Forms.MessageBox]::Show("${message}", "${title}", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
-  `;
-
-  execSync(`powershell -command "${psScript}"`, (error, stdout, stderr) => {
-    if (error) {
-      console.error("Error:", error);
-    }
-  });
-}
+const SUCCESS_ICON = `${CURRENT_FOLDER}/icons/success.png`;
+const ERROR_ICON = `${CURRENT_FOLDER}/icons/error.png`;
 
 function updateWorld() {
   try {
     execSync(`git pull ${REPO_URL}`);
-
-    const files = readdirSync(`${CURRENT_FOLDER}/worlds_local`);
-    for (const file of files) {
-      copyFileSync(
-        `${CURRENT_FOLDER}/worlds_local/${file}`,
-        `${WORLDS_LOCAL_PATH}/${file}`
-      );
+    console.log("pulling");
+    if (!fs.existsSync(`${CURRENT_FOLDER}/worlds_local`)) {
+      fs.mkdirSync(`${CURRENT_FOLDER}/worlds_local`);
     }
-    showNotification("Success", "Files are synced");
+    const files = readdirSync(`${CURRENT_FOLDER}/worlds_local`);
+    console.log("files: ", files);
+    if (files.length) {
+      for (const file of files) {
+        copyFileSync(
+          `${CURRENT_FOLDER}/worlds_local/${file}`,
+          `${WORLDS_LOCAL_PATH}/${file}`
+        );
+      }
+    }
+    notifier.notify({
+      title: "Успішно",
+      message: "Світи обновились",
+      icon: SUCCESS_ICON,
+    });
   } catch (error) {
     console.error("Error:", error);
-    showNotification("Error", error);
+    notifier.notify({
+      title: "Error",
+      message: error,
+      icon: ERROR_ICON,
+    });
   }
 }
 
@@ -60,14 +64,23 @@ async function copyAndPushWorld() {
         await execAsync("git add .");
         await execAsync('git commit -m "Сохранение мира Valheim"');
         await execAsync(`git push ${REPO_URL}`);
-        showNotification("Success", "Files are pushed");
+
+        notifier.notify({
+          title: "Успішно",
+          message: "Світи вже на гітхабі :)",
+          icon: SUCCESS_ICON,
+        });
         break;
       }
       await new Promise((resolve) => setTimeout(resolve, 1000));
     }
   } catch (error) {
     console.error("Error:", error);
-    showNotification("Error", error);
+    notifier.notify({
+      title: "Error",
+      message: error,
+      icon: ERROR_ICON,
+    });
   }
 }
 
