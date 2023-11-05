98d9d9abdd39fc4ffc851597b3c0fa5151cd7a7d
yzhura <zhuravlev.yaroslav@gmail.com>
2023-11-05 17:19:19 +0200

    rrr

--- ValheimPullWords.bat
+++ ValheimPullWords.bat
@@ -26,13 +26,11 @@
 ::ZQ0/vhVqMQ3MEVWAtB9wSA==
 ::Zg8zqx1/OA3MEVWAtB9wSA==
 ::dhA7pRFwIByZRRnk
-::Zh4grVQjdCqDJEmW+0g1Kw9HcBKDMGO/D7hR+/H17Naxo0ERXOc2db/O1ri5L/If7gvhbZNN
+::Zh4grVQjdCqDJEmW+0g1Kw9HcBKDMGO/D7hR+/H17Naxo0EVUesySJrX1oOBMuQApED8cPY=
 ::YB416Ek+ZG8=
 ::
 ::
 ::978f952a14a936cc963da21a135fa983
 @echo off
-setlocal
 cd /d %~dp0
 node pull.js
-endlocal
